7b64a32d8727c4df8585306e18c25a62756c71ac
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-07-29 18:02:18 +0530

    [90] [Time Beats: 99.90%] [Memory Beats: 71.74%] - LeetPush

--- 90-Subsets-II.java
+++ 90-Subsets-II.java
@@ -1,35 +1,37 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-       Arrays.sort(nums);
-      List<List<Integer>> j=new ArrayList<>();
-      List<Integer>l1=new ArrayList<Integer>();
-      j.add(new ArrayList<>());
-
-      gen(nums,0,l1,j);
-      return j;
+        
+        Arrays.sort(nums);
+        List<List<Integer>>l1=new ArrayList<>();
+        List<Integer> l2=new ArrayList<>();
+    l1.add(new ArrayList<Integer>());
+        check(l1,l2,0,nums);
+        return l1;
     }
-    public static void gen(int nums[],int i,List<Integer> l1 ,List<List<Integer>> j){
 
 
-   if(i==nums.length){
-    List<Integer> l3=new ArrayList<>(l1);
-    j.add(l3);
-    return;
-
-   }
-    
-   
-
-
-  l1.add(nums[i]);
-  gen(nums,i+1,l1,j);
-
-  while(i<nums.length-1 && nums[i]==nums[i+1]){
-    i++;
-  }
-  l1.remove(l1.size()-1);
-  gen(nums,i+1,l1,j);
+    public static void check(List<List<Integer>> l1,List<Integer> l2,int i,int[]n){
+        
+        if(i==n.length)
+        {
+            return;
+        }
 
         
+
+        
+      
+        l2.add(n[i]);
+        List<Integer> l3=new ArrayList<>(l2);
+         l1.add(l3);
+        check(l1,l2,i+1,n);
+        while(i<n.length-1 && n[i] ==n[i+1]){
+            i++;
+        }
+
+        l2.remove(l2.size()-1);
+        check(l1,l2,i+1,n);
+
+
     }
 }
