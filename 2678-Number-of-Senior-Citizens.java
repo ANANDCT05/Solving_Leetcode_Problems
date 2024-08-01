afc37148839b9b6566faaae464c32a5f9592e62b
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-08-01 07:41:10 +0530

    [2678] [Time Beats: 6.23%] [Memory Beats: 21.29%] - LeetPush

---
+++ 2678-Number-of-Senior-Citizens.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count=0;
+        for(int i=0;i<details.length;i++){
+            if(Integer.parseInt(details[i].substring(11,13))>=61 ){
+                count++;
+                System.out.println(details[i].charAt(11));
+            }
+        }
+
+        return count;
+    }
+}
