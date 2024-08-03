05fb2f81d67142ec19eb5915cd1923957e470c95
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-08-03 10:21:46 +0530

    [1460] [Time Beats: 100.00%] [Memory Beats: 56.26%] - LeetPush

--- 1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.java
+++ 1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
         
-        HashMap<Integer,Integer>h1=new HashMap<>();
-
+        int ht[]=new int[1001];
         for(int i=0;i<arr.length;i++){
-            h1.put(target[i],h1.getOrDefault(target[i],0)+1);
+           ht[target[i]]++;
         }
 
         for(int i=0;i<arr.length;i++){
-            if(h1.get(arr[i])==null || h1.get(arr[i])==0){
-                return false;
-            }
-            h1.put(arr[i],h1.get(arr[i])-1); 
+          if(ht[arr[i]]==0){
+           return false; 
+          }
+          ht[arr[i]]--;
         }
         return true;
     }
