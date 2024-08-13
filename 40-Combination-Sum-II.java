634f875533fb6605c77a3d24087e5230a61a2821
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-08-13 07:11:32 +0530

    [40] [Time Beats: 21.54%] [Memory Beats: 71.22%] - LeetPush

--- 40-Combination-Sum-II.java
+++ 40-Combination-Sum-II.java
@@ -1,47 +1,36 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
-       Arrays.sort(candidates);
-       
- 
         List<List<Integer>> l1=new ArrayList<>();
         List<Integer> l2=new ArrayList<>();
-        
-        check_Combination(l1,l2,target,candidates,0,0);
+Arrays.sort(candidates);
+        check(l1,l2,candidates,0,target,0);
         return l1;
     }
 
-    public static void  check_Combination(List<List<Integer>> l1,List<Integer> l2,int t,int[]c,int sum,int i){
-       
-         if(sum==t){
-            List<Integer>l3=new ArrayList<>(l2);
-            l1.add(l3);
-            return;
-        }
-        if(sum>t){
-            return ;
-        }
-       
-         if(i>=c.length){
-            return ;
-         }
-         
-        
-         
-        
+    public static void check(List<List<Integer>> l1,List<Integer> l2,int c[],int sum,int t,int i){
 
-        sum+=c[i];
-        l2.add(c[i]);
-       check_Combination(l1,l2,t,c,sum,i+1);
-       while(i<c.length-1 && c[i]==c[i+1]){
+    if(sum==t){
+        List<Integer> l3=new ArrayList<>(l2);
+     l1.add(l3);
+     return;
+    }
+    if(sum>t){
+        return;
+    }
+
+    if(i==c.length){
+        return ;
+    }
+
+
+    sum+=c[i];
+    l2.add(c[i]);
+    check(l1,l2,c,sum,t,i+1);
+    while(i<c.length-1 && c[i]==c[i+1]){
         i++;
-       }
-        l2.remove(l2.size()-1);
-        sum-=c[i];
-        
-         
-        check_Combination(l1,l2,t,c,sum,i+1);
-        
-
+    }
+    sum=sum-c[i];
+    l2.remove(l2.size()-1);
+    check(l1,l2,c,sum,t,i+1);
     }
 }
