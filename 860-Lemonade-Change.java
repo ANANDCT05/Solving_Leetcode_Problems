76451df074fb699deefc456823dd9efba2974335
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-08-15 11:43:28 +0530

    [860] [Time Beats: 14.05%] [Memory Beats: 48.09%] - LeetPush

---
+++ 860-Lemonade-Change.java
@@ -0,0 +1,64 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+       int f=0;
+       int t=0;
+       int tw=0;
+
+       for(int i=0;i<bills.length;i++){
+
+    if(bills[i]==5){
+        f++;
+    }
+    else if(bills[i]==10){
+        t++;
+    }
+    
+    int r=bills[i]-5;
+     
+       if(r==15){
+        if(f<1 ){
+           return false;
+        }
+
+        if(t<1){
+            if(f<3){
+                return false;
+            }
+
+            f=f-3;
+        }
+        else{
+            f--;
+            t--;
+        }
+
+        
+       }
+       else if(r==10){
+        if(t<1 && f<2 ){
+            return false;
+        }
+        if(t>=1){
+            t--;
+        }
+        else{
+            f--;
+            f--;
+        }
+       }
+       else if(r==5){
+        if(f<1){
+            return false;
+        }
+        f--;
+       }
+
+       }
+
+       return true;
+
+
+          
+            
+        }
+    }
