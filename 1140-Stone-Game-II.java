b2e8c38aac943eaea217f8cda7d670ce37ca65d4
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-08-20 15:42:13 +0530

    [1140] [Time Beats: 6.80%] [Memory Beats: 32.04%] - LeetPush

--- 1140-Stone-Game-II.java
+++ 1140-Stone-Game-II.java
@@ -1,80 +1,69 @@
 class Solution {
     public int stoneGameII(int[] piles) {
-        int tot=0;
+        
+     
+int dp[][]=new int[piles.length][2*piles.length];
+int dp2[][]=new int[piles.length][2*piles.length];
+    
 
-        for(int i=0;i<piles.length;i++){
-            tot=tot+piles[i];
-        }
+   return check(1,piles,1,0,dp,dp2);
+    
+}
 
-        int m=1;
-        int t=1;
-int dp[][]=new int[piles.length][piles.length*2];
-int dp2[][]=new int[piles.length][piles.length*2];
-       int a= check(piles,t,m,0,dp,dp2);
-       return tot-a;
+
+public static int check(int t,int []p,int m,int i,int[][]dp,int [][]dp2){
+
+    if(i>=p.length){
+        return 0;
     }
 
-
-    public static int check(int piles[],int t,int m,int i,int[][]dp,int dp2[][]){
-     
-
-     if(i>=piles.length){
-        return 0;
-     }
-
-
-     if(t==1){
-        int l=m;
-        if(dp2[i][l]!=0){
-            return dp2[i][l];
-        }
-     
-       
-        int ma=2*m;
+    if(t==1){
+if(dp[i][m]!=0){
+    return dp[i][m];
+}
+        int f=2*m;
         int c=1;
-        int min=Integer.MAX_VALUE;
-        while(c<=ma){
-         m=Math.max(c,m);
-         int ans=check(piles,0,m,i+c,dp,dp2);
-         min=Math.min(ans,min);
+        int ans=Integer.MIN_VALUE;
+        int sum=0;
+        int j=i;
+        while(c<=f){
+           m=Math.max(m,c);
+         if(j<p.length){
+            sum=sum+p[j];
+            int e=check(0,p,m,i+c,dp,dp2);
+            ans=Math.max(sum+e,ans);
+            j++;
+         }
+         else{
+            break;
+         }
+        
          c++;
         }
-
-        return dp2[i][l]=min;
-     }
-
-     if(t==0){
-
-        if(dp[i][m]!=0){
-            return dp[i][m];
-        }
-      int l=m;
-       int ma=2*m;
-       int c=1;
-       int sum=0;
-       int max=Integer.MIN_VALUE;
-       int j=i;
-       int ans=0;
-       while(c<=ma){
-        m=Math.max(c,m);
-       if(j<piles.length){
-      sum=sum+piles[j];
-      ans=check(piles,1,m,i+c,dp,dp2);
-
-      max=Math.max(sum+ans,max);
-
-      j++;
-       }
-       c++;
-       }
-
-    
-return dp[i][l]=max;
-     }
-
-     return 0;
+        return dp[i][f/2]=ans;
+        
     }
 
-    
 
+    if(t==0){
+        if(dp2[i][m]!=0){
+            return dp2[i][m];
+        }
+        int f=2*m;
+        int c=1;
+        int ans=Integer.MAX_VALUE;
+        
+        
+    
+       while(c<=f){
+           m=Math.max(m,c);
+         int sum=check(1,p,m,i+c,dp,dp2);
+          ans=Math.min(ans,sum);
+          c++;
+       }
+return dp2[i][f/2]=ans;
+    }
+
+    return 0;
+}
 }
