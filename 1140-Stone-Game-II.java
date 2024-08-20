52580ecdb3596f864b6916599b855d8656c3f3bc
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-08-21 01:19:48 +0530

    [1140] [Time Beats: 45.80%] [Memory Beats: 33.81%] - LeetPush

--- 1140-Stone-Game-II.java
+++ 1140-Stone-Game-II.java
@@ -1,24 +1,22 @@
 class Solution {
+    public static Integer dp[][];
+    public static Integer dp2[][];
     public int stoneGameII(int[] piles) {
-        
-     
-int dp[][]=new int[piles.length][2*piles.length];
-int dp2[][]=new int[piles.length][2*piles.length];
-    
-
-   return check(1,piles,1,0,dp,dp2);
-    
+        int size = piles.length;
+   dp = new Integer[size][size+1];
+    dp2 = new Integer[size][size+1];
+   
+   return check(1,piles,1,0);
 }
 
-
-public static int check(int t,int []p,int m,int i,int[][]dp,int [][]dp2){
+public static int check(int t,int []p,int m,int i){
 
     if(i>=p.length){
         return 0;
     }
 
     if(t==1){
-if(dp[i][m]!=0){
+    if(dp[i][m]!=null){
     return dp[i][m];
 }
         int f=2*m;
@@ -30,7 +28,7 @@ if(dp[i][m]!=0){
            m=Math.max(m,c);
          if(j<p.length){
             sum=sum+p[j];
-            int e=check(0,p,m,i+c,dp,dp2);
+            int e=check(0,p,m,i+c);
             ans=Math.max(sum+e,ans);
             j++;
          }
@@ -46,7 +44,7 @@ if(dp[i][m]!=0){
 
 
     if(t==0){
-        if(dp2[i][m]!=0){
+        if(dp2[i][m]!=null){
             return dp2[i][m];
         }
         int f=2*m;
@@ -57,7 +55,7 @@ if(dp[i][m]!=0){
     
        while(c<=f){
            m=Math.max(m,c);
-         int sum=check(1,p,m,i+c,dp,dp2);
+         int sum=check(1,p,m,i+c);
           ans=Math.min(ans,sum);
           c++;
        }
