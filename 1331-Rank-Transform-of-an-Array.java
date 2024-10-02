c63a29fe0894c4bc87e08fa4d6f4a25a8f78795d
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-10-02 15:04:09 +0530

    [1331] [Time Beats: 98.60%] [Memory Beats: 63.80%] - LeetPush

---
+++ 1331-Rank-Transform-of-an-Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+
+
+   int t[] = Arrays.copyOf(arr,arr.length);
+
+   Arrays.sort(t);
+   HashMap<Integer,Integer> h1=new HashMap<>();
+   int c=1;
+   for(int i=0;i<t.length;i++){
+    h1.put(t[i],c);
+
+    while(i<arr.length-1 && t[i]==t[i+1]){
+        i++;
+    }
+
+    c++;
+   }
+
+
+   for(int i=0;i<arr.length;i++){
+
+arr[i]=h1.get(arr[i]);
+   }
+
+   return arr;
+    }
+}
