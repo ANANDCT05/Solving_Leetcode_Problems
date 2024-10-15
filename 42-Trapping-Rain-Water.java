cc05cdfc985c63df8e3def73cae5b8d259fb1ec6
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-10-15 21:14:19 +0530

    [42] [Time Beats: 67.19%] [Memory Beats: 65.43%] - LeetPush

---
+++ 42-Trapping-Rain-Water.java
@@ -0,0 +1,90 @@
+class Solution {
+    public int trap(int[] height) {
+        int st[]=new int[height.length];
+        int c=0;
+
+      int t=0;
+
+      st[0]=height[height.length-1];
+      int l=st[0];
+int i=height.length-2;
+      while(i>=0){
+    if(height[i]>=l  ){
+      
+
+     if(t>0){
+      int n=Math.min(height[i],l);
+      while(t>0){
+        c+= n-st[t];
+        t--;
+      }
+      st[0]=height[i];
+      l=height[i];
+     }
+     else{
+        if(l==height[i] ){
+        st[++t]=height[i];
+        }
+
+        else{
+            t=0;
+         st[t]=height[i];
+         l=st[t];
+        }
+
+     }
+
+    }
+
+    else{
+        st[++t]=height[i];
+    }
+    i--;
+      }
+
+      
+      l=st[t];
+      
+
+      int st1[]=new int [height.length];
+      int t1=0;
+      st1[t1]=st[t];
+      t--;
+      while(t>=0){
+       
+        if(st[t]>=l){
+
+            if(t1>0){
+             int m=Math.min(st[t],l);
+             while(t1>0){
+                c+=(m-st1[t1]);
+                t1--;
+             }
+
+             st1[0]=st[t];
+             l=st[t];
+
+            }
+
+
+            else{
+        
+            if(l==st[t]){
+                st1[++t1]=st[t];
+            }
+            else{
+                t1=0;
+                st1[t1]=st[t];
+                l=st[t];
+            }
+            }
+        }
+
+        else{
+            st1[++t1]=st[t];
+        }
+        t--;
+      }
+      return c;
+    }
+}
