a4375363f41c4ca380597818c70001226d3676c9
ANANDCT05 <156924451+ANANDCT05@users.noreply.github.com>
2024-12-13 22:16:41 +0530

    [2593] [Time Beats: 98.53%] [Memory Beats: 58.33%] - LeetPush

---
+++ 2593-Find-Score-of-an-Array-After-Marking-All-Elements.java
@@ -0,0 +1,67 @@
+class Solution {
+    public long findScore(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        long s = 0;
+        int p = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != Integer.MAX_VALUE) {
+                if (i < nums.length - 1 && nums[i] <= p && nums[i] <= nums[i + 1]) {
+
+                    s = s + nums[i];
+                    nums[i + 1] = Integer.MAX_VALUE;
+                    if (i - 1 >= 0) {
+                        nums[i - 1] = Integer.MAX_VALUE;
+                    }
+                    nums[i] = Integer.MAX_VALUE;
+                }
+
+                else if (i == nums.length - 1) {
+
+                    if (nums[i] <= p) {
+                        s = s + nums[i];
+
+                        nums[i - 1] = Integer.MAX_VALUE;
+                        nums[i] = Integer.MAX_VALUE;
+                    }
+                }
+
+            }
+
+            p = nums[i];
+
+        }
+
+        p = Integer.MAX_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            if (nums[i] != Integer.MAX_VALUE) {
+                if (i > 0 && nums[i] <= p && nums[i] <= nums[i - 1]) {
+
+                    s = s + nums[i];
+                    nums[i - 1] = Integer.MAX_VALUE;
+                    if (i + 1 < nums.length) {
+                        nums[i + 1] = Integer.MAX_VALUE;
+                    }
+                    nums[i] = Integer.MAX_VALUE;
+                } else if (i == 0) {
+
+                    if (nums[i] <= p) {
+                        s = s + nums[i];
+
+                        nums[i] = Integer.MAX_VALUE;
+
+                    }
+                }
+
+            }
+
+            p = nums[i];
+        }
+
+        return s;
+
+    }
+}
