f3ce142c704084cb55a5fe300fa3b1e0057efa81
Anand CT <156924451+ANANDCT05@users.noreply.github.com>
2025-01-06 13:32:47 +0530

    Time: 4 ms (76.56%), Space: 44.7 MB (65.63%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,38 +1,31 @@
 class Solution {
     public int[] minOperations(String boxes) {
-    
 
-    int pref[]=new int[boxes.length()];
-    int suf[]=new int[boxes.length()];
-    int one=0;
-    int ope=0;
-    int oner=0;
-    int oper=0;
-    int j=boxes.length()-1;
 
-    for(int i=0;i<boxes.length() && j>=0 ;i++){
-   ope+=one;
-        pref[i]=ope;
+       int one=0;
+       int ope=0;
+       int oner=0;
+       int oper=0;
+       int n=boxes.length()-1;
+       int j=n;
+       int ans[]=new int[n+1];
+       for(int i=0;i<=n;i++){
+
+        ope+=one;
         if(boxes.charAt(i)=='1'){
             one+=1;
         }
-oper+=oner;
-        suf[j]=oper;
+
+        ans[i]+=ope;
+        oper+=oner;
         if(boxes.charAt(j)=='1'){
             oner+=1;
         }
+
+        ans[j]+=oper;
         j--;
-    }
-    int ans[]=new int [boxes.length()];
-ans[0]=suf[0];
-ans[boxes.length()-1]=pref[boxes.length()-1];
-    for(int i=1;i<boxes.length()-1;i++){
-         ans[i]=pref[i]+suf[i];
-    }
+       } 
 
-    return ans;
-
-
-        
+       return ans;
     }
 }
