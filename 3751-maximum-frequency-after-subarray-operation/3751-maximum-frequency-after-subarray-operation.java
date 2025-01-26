e0d4f02a93736df658118c14e936eee91c784c1e
Anand CT <156924451+ANANDCT05@users.noreply.github.com>
2025-01-27 00:25:44 +0530

    Time: 45 ms (100%), Space: 56.7 MB (100%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.java
@@ -1,11 +1,6 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-
-        
-   
-        
-
-            int pref[]=new int[nums.length];
+        int pref[]=new int[nums.length];
         int suf[]=new int[nums.length];
         HashMap<Integer,Integer>h1=new HashMap<>();
         
@@ -14,7 +9,7 @@ class Solution {
         int j=nums.length-1;
         
         for(int i=0;i<nums.length;i++){
-   h1.put(nums[i],h1.getOrDefault(nums[i],0)+1);
+   h1.put(nums[i],i);
             pref[i]=count;
             if(nums[i]==k){
                 count++;
@@ -25,27 +20,21 @@ class Solution {
             }
             j--;
         }
-        
     int ans=pref[nums.length-1];
         if(nums[nums.length-1]==k){
             ans++;
         }
-        
 int tot=ans;
     for(Map.Entry<Integer,Integer> m1:h1.entrySet()){
-
         int key=m1.getKey();
      if(key!=k){
-
           int val=0;
             int cur=0;
             int i=0;
              j=0;
              int v=0;
             while(j<nums.length){
-
                  if(nums[j]==k){
-                   
                     cur--;
                  }
                if(nums[j]==key){
@@ -67,16 +56,9 @@ int tot=ans;
             }
      
             ans=Math.max(val,ans);
-     }
-            
-            
-            
-            
-        
+     }  
     }
-
-        return ans;
-        
+        return ans;   
         }
         
     }
