c9b6f839522fde309109425479527f670d8995d8
Anand CT <156924451+ANANDCT05@users.noreply.github.com>
2025-02-05 13:40:53 +0530

    Time: 12 ms (46.07%), Space: 44.8 MB (30.19%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int swimInWater(int[][] grid) {
+        
+        int d[][]=new int[grid.length][grid.length];
+        for(int m[]:d){
+            Arrays.fill(m,Integer.MAX_VALUE);
+        }
+
+    int ro[]={1,-1,0,0};
+    int co[]={0,0,1,-1};
+   Comparator<int[]> c1=new Comparator<>(){
+        public int compare(int a[],int b[]){
+
+        if(a[2]>b[2]){
+            return 1;
+        }
+        return -1;
+    }
+   };
+   PriorityQueue<int[]>p1=new PriorityQueue<>(c1);
+   d[0][0]=grid[0][0];
+   p1.add(new int[]{0,0,d[0][0]});
+   while(!p1.isEmpty()){
+    int arr[]=p1.poll();
+
+    for(int j=0;j<4;j++){
+        int nx=arr[0]+ro[j];
+        int ny=arr[1]+co[j];
+        if(nx>=0 && ny>=0 && nx<grid.length && ny<grid.length ){
+
+            int max=Math.max(grid[nx][ny],arr[2]);
+
+            if(d[nx][ny]>max){
+                d[nx][ny]=max;
+                p1.add(new int[]{nx,ny,max});
+            }
+        }
+    }
+
+   }
+
+   return d[grid.length-1][grid.length-1];
+
+        
+    }
+}
