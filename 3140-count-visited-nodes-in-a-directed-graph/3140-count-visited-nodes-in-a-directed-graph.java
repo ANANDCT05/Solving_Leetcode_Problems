ee942d39577f5a2bd8de13d5eb7424e9240247f2
Anand CT <156924451+ANANDCT05@users.noreply.github.com>
2025-02-13 15:09:25 +0530

    Time: 1370 ms (5.95%), Space: 74.4 MB (40.48%) - LeetHub

---
+++ 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] countVisitedNodes(List<Integer> edges) {
+        
+
+        int v[]=new int[edges.size()];
+
+        int ans[]=new int[edges.size()];
+        for(int i=0;i<edges.size();i++){
+            if(v[i]==0){
+
+                check(v,edges,ans,1,i);
+            }
+        }
+
+        return ans;
+    }
+
+
+    public static int check(int v[],List<Integer>l1,int ans[],int c,int i){
+
+        if(ans[i]!=0){
+            return ans[i];
+        }
+
+        if(v[i]!=0){
+
+            int cycle=c-v[i];
+            v[i]=-1;
+            return -cycle;
+        }
+  v[i]=c;
+        int anss=check(v,l1,ans,c+1,l1.get(i));
+        if(anss<0){
+            ans[i]=-anss;
+
+              if(v[i]==-1){
+            return -anss;
+        }
+        else{
+            return anss;
+        }
+        }
+       
+        if(anss>0){
+            ans[i]=anss+1;
+            System.out.println(i);
+
+        }
+
+        return ans[i];
+
+    
+    }
+}
