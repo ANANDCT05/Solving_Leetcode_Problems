a1a64601c143e9c39c262959e3d0bd20e5b2942f
Anand CT <156924451+ANANDCT05@users.noreply.github.com>
2025-02-27 09:33:05 +0530

    Time: 151 ms (70.71%), Space: 45.3 MB (68.34%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.java
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.java
@@ -1,18 +1,17 @@
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
-       HashMap<Long,Integer>h1=new HashMap<>();
+       HashMap<Integer,Integer>h1=new HashMap<>();
        for(int i=0;i<arr.length;i++){
-        h1.put((long)arr[i],1);
+        h1.put(arr[i],1);
        }
 int max=0;
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
 
-                long f1=arr[i];
                 long f2=arr[j];
                 int c=2;
                 long s=arr[i]+arr[j];
-                while(h1.get(s)!=null){
+                while(s<=1000000000 && h1.get((int)s)!=null){
                     c++;
                     long t=s;
                     s=f2+s;
